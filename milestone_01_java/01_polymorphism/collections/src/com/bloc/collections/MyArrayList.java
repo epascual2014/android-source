844160ac6c5d54d3be583b7e941b7a5a4fd29998
Edrick Pascual <edrickpascual@yahoo.com>
2014-05-20 23:23:01 -0700

    collections tough

--- milestone_01_java/01_polymorphism/collections/src/com/bloc/collections/MyArrayList.java
+++ milestone_01_java/01_polymorphism/collections/src/com/bloc/collections/MyArrayList.java
@@ -17,13 +17,31 @@ public class MyArrayList<E> extends Object implements List<E> {
 
 	@Override
 	public boolean add(E e) {
-		// YOUR WORK HERE
-		return false;
+		 Object[] temp = new Object[mList.length + 1];
+		 for (int i = 0; i < mList.length; ++i){
+			 temp[i] = mList[i]; // signify that temp is has the same info in mList
+		 }
+		 temp[mList.length] = e;  // we added e to the array
+		 mList = temp; // the stored memory is in mList and we have to call it and say its in temp now.
+		 return true;// YOUR WORK HERE
 	}
 
 	@Override
 	public void add(int index, E element) {
 		// YOUR WORK HERE
+		if (index < 0 || index >= mList.length){   
+			throw new IndexOutOfBoundsException();  // check if index is ok.
+		}
+		Object[] temp = new Object[mList.length + 1];  // create the new array into temp.
+		for (int i = 0, j = 0; j < temp.length; ++i, ++j){  // loop into the array by comparing the first array to the second array's index i&j
+			if ( i == index ) {  // check if i is valid
+				temp[index] = element;  // insert element into the array
+				--i; // decrement original array to cancel out j temp array
+			} else {
+				temp[j] = mList[i]; // 
+			}
+		}
+		mList = temp;
 	}
 
 	@Override
@@ -38,12 +56,16 @@ public class MyArrayList<E> extends Object implements List<E> {
 
 	@Override
 	public void clear() {
-		// YOUR WORK HERE
+		mList = new Object[0];// YOUR WORK HERE create list size of 0.
 	}
 
 	@Override
 	public boolean contains(Object o) {
-		// YOUR WORK HERE
+		for (int i = 0; i < mList.length; ++i){ // loop around the index
+			if (mList[i].equals(o)){ // if found o return true
+				return true;  // YOUR WORK HERE
+			}
+		}
 		return false;
 	}
 
@@ -59,7 +81,8 @@ public class MyArrayList<E> extends Object implements List<E> {
 
 	@Override
 	public E get(int index) {
-		// YOUR WORK HERE
+		for (int i = 0; i < mList.length; ++i)// YOUR WORK HERE
+		return (E)mList[index]; // just returning index
 		return null;
 	}
 
@@ -70,13 +93,19 @@ public class MyArrayList<E> extends Object implements List<E> {
 
 	@Override
 	public int indexOf(Object o) {
-		// YOUR WORK HERE
-		return 0;
+		for (int i = 0; i < mList.length; ++i){ // loop around the index 
+			if (mList[i].equals (o)){
+				return i;// YOUR WORK HERE
+			}
+		}
+		return -1; // since -1 is less than array which is not possible
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// YOUR WORK HERE
+		if (mList.length <= 0 ){
+			return true;// YOUR WORK HERE
+		}
 		return false;
 	}
 
@@ -102,15 +131,29 @@ public class MyArrayList<E> extends Object implements List<E> {
 
 	@Override
 	public E remove(int index) {
-		// YOUR WORK HERE
+		Object[] temp = new Object[mList.length + 1];  // create the new array into temp.
+		for (int i = 0, j = 0; j < temp.length; --i, --j){  // loop into the array by comparing the first array to the second array's index i&j
+			if ( i == index ) {  // check if i is valid
+				++i; // increment original array to cancel out j temp array
+			} else {
+				temp[j] = mList[i]; // 
+			}
+		}
+		mList = temp;
 		return null;
 	}
 
 	@Override
 	public boolean remove(Object o) {
-		// YOUR WORK HERE
-		return false;
+		 // YOUR WORK HERE
+		for (int i = 0; i < mList.length; ++i){ // loop around the index
+			if (mList[i].equals(o)){ // if found o return true
+
+			}
+		} // YOUR WORK HERE  two variables i and j
+		return true;
 	}
+	
 
 	@Override
 	public boolean removeAll(Collection<?> c) {
@@ -130,8 +173,10 @@ public class MyArrayList<E> extends Object implements List<E> {
 
 	@Override
 	public int size() {
-		// YOUR WORK HERE
-		return 0;
+		for (int i = 0; i < mList.length; ++i){
+			mList[i] = size();// YOUR WORK HERE
+		}
+			return size();
 	}
 
 	@Override
