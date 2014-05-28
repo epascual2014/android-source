214644e3a755a0de3dbc82cc78cfc1964c2cca1c
Edrick Pascual <edrickpascual@yahoo.com>
2014-05-27 22:16:05 -0700

    Recursion

--- milestone_01_java/02_nuance/recursion/src/com/bloc/recursion/RecursionUtils.java
+++ milestone_01_java/02_nuance/recursion/src/com/bloc/recursion/RecursionUtils.java
@@ -13,8 +13,31 @@ public class RecursionUtils extends Object {
 	 *
 	 * Hint: your base case may be a comparison of 2 numbers
 	 */
+	
 	public static final int findMaxRecursively(List<Integer> numbers) {
 		// IMPLEMENT ME
-		return 0;
+		if (numbers.size() == 1){ // size of list equals to 1
+			return numbers.get(0); // returns the first element
+		} else {
+			int numStored = numbers.get(0); // store info
+			int maxOfRest = findMaxRecursively(numbers.subList(1, numbers.size())); // the max number the rest of the array not including stored list
+			return Math.max(numStored, maxOfRest);
+		}
+		
 	}
 }
+		/*
+		int count = 1, max = 1;
+		List<Integer> numbers = new List<Integer>();
+		for (int i = 1; i < numbers.length; ++i) {
+			if (numbers[i] >= numbers[i-1]) {
+				count++;
+			} else {
+				if (count > max) {
+					max = count;
+				}
+				count = 1;
+			}
+		}
+		return 0;
+	}*/
